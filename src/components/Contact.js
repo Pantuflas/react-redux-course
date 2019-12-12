ba1a46d401b9275190124425f9906870f86e7a60
Pantuflas <antoniojss94@gmail.com>
2019-12-11 20:11:26 -0600

    react-router base

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact page</h4>
+            <p>Reprehenderit aliqua aliqua velit consectetur cupidatat quis non id voluptate reprehenderit aliqua ipsum. Dolor ipsum nostrud duis sit sunt sunt pariatur deserunt duis. Labore enim et excepteur magna est. Lorem consequat commodo eu pariatur reprehenderit sit cillum tempor aute commodo. </p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home page</h4>
+            <p>Reprehenderit aliqua aliqua velit consectetur cupidatat quis non id voluptate reprehenderit aliqua ipsum. Dolor ipsum nostrud duis sit sunt sunt pariatur deserunt duis. Labore enim et excepteur magna est. Lorem consequat commodo eu pariatur reprehenderit sit cillum tempor aute commodo. </p>
+        </div>
+    )
+}
+
+export default Home;
