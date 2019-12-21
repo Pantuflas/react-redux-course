8a7e682b673cf1ce733910965f969d05fc3ebf72
José Antonio Sánchez Sayago <antoniojss94@gmail.com>
2019-12-20 19:26:37 -0600

    Working redux (#3)
    * react-redux tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import NavBar from './components/NavBar'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Post from './components/Post'
 
 function App(){
   return (
     <BrowserRouter>
       <div className="App">
         <NavBar />
-        <Route exact path='/'   component={Home} />
-        <Route path='/about'    component={About} />
-        <Route path='/contact'  component={Contact} />
+        {/* Switch component goes through all the Routes and renders the first that matches */}
+        <Switch>
+          <Route exact path='/'   component={Home} />
+          <Route path='/about'    component={About} />
+          <Route path='/contact'  component={Contact} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,21 @@
+const initState = {
+    posts: [
+        {id: '1', title: 'With love, Karen', body: 'Consectetur magna enim duis dolore ut ullamco consequat. Irure laboris est ut enim quis magna culpa. Lorem mollit aliqua duis irure elit minim minim amet et eu pariatur et.'},
+        {id: '2', title: 'Dummy title', body: 'Enim proident ex tempor consequat pariatur quis officia nulla Lorem occaecat ut laboris. Reprehenderit occaecat nisi cupidatat in est ea incididunt amet aute occaecat sit magna. Consequat labore adipisicing labore ad cupidatat aliqua commodo do exercitation anim aliquip laboris commodo amet. Aliquip laboris non amet excepteur eiusmod excepteur.'},
+        {id: '3', title: 'Muffin & Vodka', body: 'Ad laboris ut exercitation esse elit do veniam. Id laboris irure ea aute commodo eu amet magna do esse eiusmod exercitation excepteur velit. Ut ea mollit cillum minim fugiat cupidatat dolore eiusmod aliqua. Dolor commodo fugiat labore ullamco esse exercitation excepteur ea duis magna do est. Velit est dolore eiusmod do incididunt dolor irure id. Ea fugiat et ad laborum voluptate magna incididunt mollit ullamco velit ad. Incididunt consectetur do sunt ea amet reprehenderit sit ullamco mollit eu est et dolor.'}
+    ]
+}
+const rootReducer = (state = initState, action) => {
+    if(action.type === 'DELETE_POST') {
+        let newPosts = state.posts.filter(post => {
+            return action.id !== post.id
+        });
+        return {
+            ...state,
+            posts: newPosts
+        }
+    }
+    return state;
+}
+
+export default rootReducer;
